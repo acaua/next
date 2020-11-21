61a60a3e3daae9514dd08dc1b6d43ed1b8e7d028
Acaua Sperl de Faria <acaua.faria@gmail.com>
2020-11-21 03:36:33 -0300

    Hello XO and Prettier: linting and formating enabled

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
-import Head from "next/head";
+import Head from 'next/head';
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Hello world</title>
-      </Head>
-      <div>Hello world!</div>
-    </>
-  );
+	return (
+		<>
+			<Head>
+				<title>Hello world</title>
+			</Head>
+			<div>Hello world!</div>
+		</>
+	);
 }
