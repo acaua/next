ae97bb987e211113fdf48dd524503ae33569de9c
Acaua Sperl de Faria <acaua.faria@gmail.com>
2020-11-21 21:06:35 -0300

    Hello tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+	darkMode: false, // Or 'media' or 'class'
+	theme: {
+		extend: {}
+	},
+	variants: {
+		extend: {}
+	},
+	plugins: []
+};
